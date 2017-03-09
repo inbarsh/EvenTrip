14aff2bbd46c705757032245681800d001435f01
Ido Shviro <idoshviro@yahoo.com>
2017-03-10 01:28:57 +0200

    Main Activity

--- Eventrip/app/src/main/java/com/eventrip/MainActivity.java
+++ Eventrip/app/src/main/java/com/eventrip/MainActivity.java
@@ -1,13 +1,29 @@
 package com.eventrip;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout searchButton = (LinearLayout) findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.searchButton:
+                //Intent intent = new Intent(this, new MainActivity());
+                break;
+        }
     }
 }
