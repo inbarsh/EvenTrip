3394c2a12118647eda91b1ec2cbaa88228ff08bb
LAPTOPPC\U <melnik@g.jct.ac.il>
2017-03-10 07:59:07 +0200

    עירת מאפ לפי מיקום

---
+++ Eventrip/app/src/main/java/com/eventrip/map.java
@@ -0,0 +1,28 @@
+package com.eventrip;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class map extends AppCompatActivity implements OnMapReadyCallback{
+GoogleMap googleMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+       this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(46.676412,11.083514),14.0f));
+    }
+}
---
+++ Eventrip/app/src/main/java/com/eventrip/sidemenu.java
@@ -0,0 +1,13 @@
+package com.eventrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sidemenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sidemenu);
+    }
+}
